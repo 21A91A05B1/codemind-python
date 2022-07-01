0436573b7a30fc14ffabf1fc16073ac5ade4fb83
21A91A05B1 <97426126+21A91A05B1@users.noreply.github.com>
2022-07-01 21:36:59 +0530

    n,k=map(int,input().split()) arr=list(map(int,input().split())) c=0 a=[] for i in arr:     if(i<0):         i=-1     if(i%k!=0):         c+=1 print(c)

---
+++ Count_elements_not_divisible_by_K.py
@@ -0,0 +1,12 @@
+n,k=map(int,input().split())
+arr=list(map(int,input().split()))
+c=0
+a=[]
+for i in arr:
+    if(i<0):
+        i=-1
+    if(i%k!=0):
+        c+=1
+print(c)        
+        
+    
