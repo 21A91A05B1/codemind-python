294814e7eaa68bfa7517eed4c915840b71330d63
21A91A05B1 <97426126+21A91A05B1@users.noreply.github.com>
2022-07-01 20:06:58 +0530

    t=int(input()) while(t>0):     n=int(input())     for i in range(1,n+1):         if(n==i*i):             print("True")             break     else:         print("False")     t-=1

---
+++ Valid_Perfect_Square.py
@@ -0,0 +1,10 @@
+t=int(input())
+while(t>0):
+    n=int(input())
+    for i in range(1,n+1):
+        if(n==i*i):
+            print("True")
+            break
+    else:
+        print("False")
+    t-=1    
